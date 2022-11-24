adeb76bc1a34257c1ae84e13fb8475628cc97c30
Gettz <49850415+Gettz@users.noreply.github.com>
2022-11-24 15:14:20 -0600

    Update GenesysForm.user.js

--- GenesysForm.user.js
+++ GenesysForm.user.js
@@ -17,7 +17,6 @@ fillNode.innerHTML = '<button id="myButton" type="button"> Set Name</button>';
 fillNode.setAttribute ('id', 'myBContainer');
 document.body.appendChild(fillNode);
 
-
 var myModal = document.createElement ('div');
 myModal.innerHTML = '                                                    \
 <div id="myModal" class="modal">                                         \
